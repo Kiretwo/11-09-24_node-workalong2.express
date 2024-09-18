928cd2c5f07b9ddecf9d2409db0573b0992468b4
Kiretwo <eritor00991@stud.noroff.no>
2024-09-18 13:54:49 +0200

    update

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const PORT = 3001;
 const homeRoute = require('./routes/home');
 const aboutRoute = require('./routes/about');
 const contactRoute = require('./routes/contact');
+const usersRoute = require('./routes/users'); // Import the new users route
 // Middleware to parse request bodies (if needed)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/', homeRoute); // Homepage route
 app.use('/about', aboutRoute); // About page route
 app.use('/contact', contactRoute); // Contact page route
+app.use('/users', usersRoute); // Users page route (fetches data from API)
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
